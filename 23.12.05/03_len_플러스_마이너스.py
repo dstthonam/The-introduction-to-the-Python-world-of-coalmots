1ce6b8da446abc6d4ba1f8f08339f003d4dd4471
dstthonam <thonam123@naver.com>
2023-12-05 20:02:01 +0900

    40.12.05
    1교시 후 저장

---
+++ 23.12.05/03_len_플러스_마이너스.py
@@ -0,0 +1,13 @@
+
+len("hello")
+len("hello python")
+
+name = "박인성"
+len(name)
+
+cheer = "파이팅" + "파이팅" + "파이팅"
+cheer
+
+cheer2 = "파이팅" * 3
+cheer2
+
---
+++ 23.12.05/04.py
@@ -0,0 +1,5 @@
+word = "Python"
+print(word)
+word.upper()
+word.lower()
+
